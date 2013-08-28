4d5c59a259c42b28cbcc29fb27324f243ba74c4b
Evgeny Stambulchik <fnevgeny@gmail.com>
2013-08-28 12:19:26 +0000

    Output absolute level energies by default.

--- cfac_schema.sql
+++ cfac_schema.sql
@@ -12,7 +12,7 @@ CREATE TEMPORARY VIEW _cstates_v AS
     GROUP BY l.nele;
 
 CREATE TEMPORARY VIEW _levels_v AS
-  SELECT l.sid, l.id, l.name, l.nele, cs.zsp, l.e - cs.e_gs AS e,
+  SELECT l.sid, l.id, l.name, l.nele, cs.zsp, l.e, l.e - cs.e_gs AS e_rel,
          l.g, l.vn, l.vl, l.p, l.ncomplex, l.sname
     FROM levels AS l
       INNER JOIN _cstates_v AS cs ON (l.sid = cs.sid AND l.nele = cs.nele);

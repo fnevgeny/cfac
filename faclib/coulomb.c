6e16ca0379b46f895cb36be644047a374f071ade
Evgeny Stabmulchik <fnevgeny@gmail.com>
2015-07-09 15:22:39 +0000

    Explicitly include headers that were included indirectly via cf77.h &co.

--- faclib/coulomb.c
+++ faclib/coulomb.c
@@ -24,6 +24,9 @@
   Author: M. F. Gu, mfgu@stanford.edu
 **************************************************************/
 
+#include <stdlib.h>
+#include <stdio.h>
+#include <string.h>
 #include <math.h>
 #include <gsl/gsl_sf_gamma.h>
 

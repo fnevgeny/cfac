242eaece6cedb77ff76f13a0920afd0a67300bb7
Evgeny Stabmulchik <fnevgeny@gmail.com>
2015-07-09 13:40:36 +0000

    Moved clogam.f and f11.f contents inside coulcc.f.

--- coul/coulcc.f
+++ coul/coulcc.f
@@ -902,6 +902,167 @@ C     X=', 0P,2F8.3)
       RETURN
       END
 
+      FUNCTION F11(X,ETA,ZL,P,EPS,LIMIT,KIND,ERR,NITS,FPMAX,ACC8,ACC16)
+      IMPLICIT REAL*8(A-H,O-Z)
+      COMPLEX*16 X,ETA,ZL,P,AA,BB,Z,F11,CDIGAM,CI
+      COMPLEX*16 DD,G,F,AI2,BI2,T2
+      LOGICAL ZLLIN
+
+      INTEGER MPDIM
+      PARAMETER (MPDIM = 2)
+
+      REAL*8 AR(MPDIM), BR(MPDIM), GR(MPDIM), GI(MPDIM), DR(MPDIM)
+      REAL*8 DI(MPDIM), TR(MPDIM), TI(MPDIM), UR(MPDIM), UI(MPDIM)
+      REAL*8 FI(MPDIM), FI1(MPDIM), MPONE(MPDIM)
+      REAL*8 AI(MPDIM), BI(MPDIM), DEN(MPDIM), TR2(MPDIM)
+      REAL*8 TI2(MPDIM), MPTMP(MPDIM), ZI(MPDIM), ZR(MPDIM)
+
+      DATA ZERO,ONE,TWO / 0D+0, 1D+0, 2D+0 /, CI / (0D+0, 1D+0) /
+      
+      ABSC(AA) = ABS(DBLE(AA)) + ABS(IMAG(AA))
+      NINTC(AA) = NINT(REAL(REAL(AA)))
+
+      F11 = ZERO
+C
+C *** evaluate the HYPERGEOMETRIC FUNCTION 1F1
+C                                        i
+C            F (AA;BB; Z) = SUM  (AA)   Z / ( (BB)  i] )
+C           1 1              i       i            i
+C
+C     to accuracy EPS with at most LIMIT terms.
+C  If KIND = 0 : using extended precision but real arithmetic only,
+C            1 : using normal precision in complex arithmetic,
+C   or       2 : using normal complex arithmetic, but with CDIGAM factor
+C
+C  where
+         AA = ZL+ONE - ETA*P
+         BB = TWO*(ZL+ONE)
+C  and
+         Z  = TWO*P*X
+C
+         ZLLIN = DBLE(BB).LE.ZERO .AND. ABS(BB-NINTC(BB)).LT.ACC8**0.25
+             IF(.NOT.ZLLIN.OR.DBLE(BB)+LIMIT.LT.1.5) GO TO 10
+                NITS = -1
+                RETURN
+   10 IF(LIMIT.LE.0) THEN
+         F11 = ZERO
+         ERR = ZERO
+         NITS= 1
+         RETURN
+         ENDIF
+      TA = ONE
+      RK = ONE
+      IF(KIND.LE.0.AND.ABSC(Z)*ABSC(AA).GT.ABSC(BB) * 1.0) THEN
+         CALL MPDMC(ONE, 0D0, DR)
+         CALL MPDMC(ZERO, 0D0, DI)
+         CALL MPDMC(ONE, 0D0, GR)
+         CALL MPDMC(ZERO, 0D0, GI)
+         CALL MPDMC(IMAG(AA), 0D0, AI)
+         CALL MPDMC(DBLE(AA), 0D0, AR)
+         CALL MPDMC(IMAG(BB), 0D0, BI)
+         CALL MPDMC(DBLE(BB), 0D0, BR)
+         CALL MPDMC(ZERO, 0D0, FI)
+         CALL MPDMC(ONE, 0D0, MPONE)
+         CALL MPDMC(DBLE(Z), 0D0, ZR)
+         CALL MPDMC(IMAG(Z), 0D0, ZI)
+         
+      DO 20 I=2,LIMIT
+         CALL MPADD(FI, MPONE, FI1)
+
+         CALL MPMUL(BR, FI1, TR)
+         CALL MPMUL(BI, FI1, TI)
+
+         CALL MPMUL(TR, TR, TR2)
+         CALL MPMUL(TI, TR, TI2)
+         CALL MPADD(TR2, TI2, MPTMP)
+         CALL MPDIV(MPONE, MPTMP, DEN)
+
+         CALL MPMUL(AR, TR, TR2)
+         CALL MPMUL(AI, TI, TI2)
+         CALL MPADD(TR2, TI2, MPTMP)
+         CALL MPMUL(MPTMP, DEN, UR)
+
+         CALL MPMUL(AI, TR, TR2)
+         CALL MPMUL(AR, TI, TI2)
+         CALL MPSUB(TR2, TI2, MPTMP)
+         CALL MPMUL(MPTMP, DEN, UI)
+
+         CALL MPMUL(UR, GR, TR2)
+         CALL MPMUL(UI, GI, TI2)
+         CALL MPSUB(TR2, TI2, TR)
+
+         CALL MPMUL(UR, GI, TR2)
+         CALL MPMUL(UI, GR, TI2)
+         CALL MPADD(TR2, TI2, TI)
+
+         CALL MPMUL(ZR, TR, TR2)
+         CALL MPMUL(ZI, TI, TI2)
+         CALL MPSUB(TR2, TI2, GR)
+
+         CALL MPMUL(ZR, TI, TR2)
+         CALL MPMUL(ZI, TR, TI2)
+         CALL MPADD(TR2, TI2, GI)
+
+         CALL MPADD(DR, GR, DR)
+         CALL MPADD(DI, GI, DI)
+         
+         CALL MPABS(GR, TR2)
+         CALL MPABS(GI, TI2)
+         CALL MPADD(TR2, TI2, MPTMP)
+         ERR = MPTMP(1) + MPTMP(2)
+         IF(ERR.GT.FPMAX) GO TO 60
+
+         CALL MPABS(DR, TR2)
+         CALL MPABS(DI, TI2)
+         CALL MPADD(TR2, TI2, MPTMP)
+         RK = MPTMP(1) + MPTMP(2)
+         TA = MAX(TA,RK)
+
+         IF(ERR.LT.RK*EPS .OR. I.GE.4.AND.ERR.LT.ACC16) GO TO 30
+
+         CALL MPEQ(FI1, FI)
+         CALL MPADD(AR, MPONE, AR)
+         CALL MPADD(BR, MPONE, BR)
+ 20      CONTINUE
+C
+ 30      F11R = DR(1) + DR(2)
+         F11I = DI(1) + DI(2)
+         F11 = DCMPLX(F11R, F11I)
+         ERR = ACC16 * TA / RK
+C
+      ELSE
+C* ---------------------------------- alternative code
+C*    If REAL*16 arithmetic is not available, (or already using it]),
+C*    then use KIND > 0
+         G = ONE
+          F = ONE
+          IF(KIND.GE.2) F = CDIGAM(AA) - CDIGAM(BB) - CDIGAM(G)
+         DD = F
+         DO 40 I=2,LIMIT
+            AI2 = AA + (I-2)
+            BI2 = BB + (I-2)
+            R  = I-ONE
+         G = G * Z * AI2 / (BI2 * R)
+         IF(KIND.GE.2)
+C                              multiply by (psi(a+r)-psi(b+r)-psi(1+r))
+     X        F = F + ONE/AI2 - ONE/BI2 - ONE/R
+         T2  = G * F
+         DD = DD + T2
+            ERR = ABSC(T2)
+               IF(ERR.GT.FPMAX) GO TO 60
+            RK = ABSC(DD)
+         TA = MAX(TA,RK)
+         IF(ERR.LT.RK*EPS.OR.ERR.LT.ACC8.AND.I.GE.4) GO TO 50
+   40    CONTINUE
+
+   50    ERR = ACC8 * TA / RK
+         F11 = DD
+C* ------------------------------------------- end of alternative code
+      ENDIF
+   60    NITS = I
+      RETURN
+      END
+
       FUNCTION CF1R(X,ETA,ZL,EPS,FCL,TPK1,ETANE0,LIMIT,ERR,NFP,
      X              ACCH,FPMIN,FPMAX,PR,CALLER)
       IMPLICIT REAL*8(A-H,O-Z)
@@ -1257,6 +1418,152 @@ C1000 FORMAT('0RCF: ZERO CF COEFFICIENT AT POSITION ',I4/)
    90 PRINT 1000,M,IBEG-1
  1000 FORMAT('0RCF: LAST CALL SET M =',I4,', BUT RESTART REQUIRES',I4)
       STOP
+      END
+
+      FUNCTION CLOGAM(Z)
+C
+C     this routine computes the logarithm of the gamma function gamma(z)
+C     for any complex argument 'Z' to any accuracy preset by CALL LOGAM
+C
+      IMPLICIT REAL*8(A-H,O-Z)
+      COMPLEX*16 Z,U,V,H,R,CLOGAM,CDIGAM,LOGAM,SER
+      DIMENSION B(15),BN(15),BD(15)
+C
+      DATA LERR /6/, NX0 /6/, NB /15/,
+     X  ZERO,ONE,TWO,FOUR,HALF,QUART /0D+0,1D+0,2D+0,4D+0,.5D+0,.25D+0/
+      DATA BN(1),BD(1)    / +1D+0,   6D+0 /,
+     X     BN(2),BD(2)    / -1D+0,  30D+0 /,
+     X     BN(3),BD(3)    / +1D+0,  42D+0 /,
+     X     BN(4),BD(4)    / -1D+0,  30D+0 /,
+     X     BN(5),BD(5)    / +5D+0,  66D+0 /,
+     X     BN(6),BD(6)    /          -691D+0,  2730D+0/,
+     X     BN(7),BD(7)    /          +  7D+0,     6D+0/,
+     X     BN(8),BD(8)    /         -3617D+0,   510D+0/,
+     X     BN(9),BD(9)    /         43867D+0,   798D+0/,
+     X     BN(10),BD(10)  /       -174611D+0,   330D+0/,
+     X     BN(11),BD(11)  /        854513D+0,   138D+0/,
+     X     BN(12),BD(12)  /    -236364091D+0,  2730D+0/,
+     X     BN(13),BD(13)  /     + 8553103D+0,     6D+0/,
+     X     BN(14),BD(14)  /  -23749461029D+0,   870D+0/,
+     X     BN(15),BD(15)  / 8615841276005D+0, 14322D+0/
+      DATA FPLMIN / -140D+0 /
+      DATA ACCUR /-1D30/
+
+      SAVE ACCUR, NT, NX0, PI, HL2P, ALPI, B
+
+C
+      X=DBLE(Z)
+      T=IMAG(Z)
+      MX = INT(DBLE(ACCUR*100 - X))
+      IF(ABS(ABS(X)-MX) + ABS(T).LT.ACCUR*50) GO TO 60
+      F=ABS(T)
+      V=DCMPLX(X,F)
+      IF(X .LT. ZERO) V=ONE-V
+      H=ZERO
+      C=DBLE(V)
+      N=NX0-INT(C)
+      IF(N .LT. 0) GO TO 30
+      H=V
+      D=IMAG(V)
+      A=ATAN2(D,C)
+      IF(N .EQ. 0) GO TO 20
+      DO 10 I = 1,N
+      C=C+ONE
+      V=DCMPLX(C,D)
+      H=H*V
+   10 A=A+ATAN2(D,C)
+   20 H=DCMPLX(HALF*LOG(DBLE(H)**2+IMAG(H)**2),A)
+      V=V+ONE
+   30 R=ONE/V**2
+      SER = B(NT)
+      DO 40 J=2,NT
+        K = NT+1 - J
+   40 SER = B(K) + R*SER
+      CLOGAM = HL2P+(V-HALF)*LOG(V)-V + SER/V - H
+      IF(X .GE. ZERO) GO TO 50
+C
+      A= INT(X)-ONE
+      C=PI*(X-A)
+      D=PI*F
+C     E=EXP(-TWO*D)
+        E = ZERO
+        F = -TWO*D
+        IF(F.GT.FPLMIN) E = EXP(F)
+      F=SIN(C)
+      E= D + HALF*LOG(E*F**2+QUART*(ONE-E)**2)
+      F=ATAN2(COS(C)*TANH(D),F)-A*PI
+      CLOGAM=ALPI-DCMPLX(E,F)-CLOGAM
+C
+   50 IF(SIGN(ONE,T) .LT. -HALF) CLOGAM=CONJG(CLOGAM)
+
+      RETURN
+C
+   60 WRITE(LERR,1000) 'CLOGAM',X
+ 1000 FORMAT(1X,A6,' ... ARGUMENT IS NON POSITIVE INTEGER = ',F20.2)
+      CLOGAM = ZERO
+      RETURN
+C
+      ENTRY CDIGAM(Z)
+C
+C     this routine computes the logarithmic derivative of the gamma
+C     function  psi(Z) = digamma(Z) = d (ln gamma(Z))/dZ  for any
+C     complex argument Z, to any accuracy preset by CALL LOGAM(ACC)
+C
+      U=Z
+      X=DBLE(U)
+      A=ABS(X)
+      IF(ABS(IMAG(U)) + ABS(A + INT(X)) .LT. ACCUR) GO TO 110
+      IF(X .LT. ZERO) U=-U
+      V=U
+      H=ZERO
+      N=NX0-INT(A)
+      IF(N .LT. 0) GO TO 90
+      H=ONE/V
+      IF(N .EQ. 0) GO TO 80
+      DO 70 I = 1,N
+      V=V+ONE
+   70 H=H+ONE/V
+   80 V=V+ONE
+   90 R=ONE/V**2
+      SER = B(NT) * (2*NT-1)
+      DO 100 J=2,NT
+        K = NT+1 - J
+  100 SER = B(K)*(2*K-1) + R*SER
+      CDIGAM = LOG(V) - HALF/V - R*SER - H
+      IF(X .GE. ZERO) RETURN
+      H=PI*U
+      CDIGAM = CDIGAM + ONE/U + PI*COS(H)/SIN(H)
+      RETURN
+C
+  110 WRITE(LERR,1000) 'CDIGAM',X
+      CDIGAM=ZERO
+      RETURN
+C
+      ENTRY LOGAM(ACC)
+C
+C      initialisation call for calculations to accuracy 'ACC'
+C
+      
+      CLOGAM = ZERO
+      
+      IF (ABS(ACC-ACCUR) .LE. ACC*1D-2) RETURN
+
+      NX0 = 6
+      X0  = NX0 + ONE
+      PI = FOUR*ATAN(ONE)
+      ALPI = LOG(PI)
+      HL2P = LOG(TWO*PI) * HALF
+      ACCUR = ACC
+      DO 120 K=1,NB
+       F21 = K*2 - ONE
+       B(K) = BN(K) / (BD(K) * K*TWO * F21)
+       ERR = ABS(B(K)) * K*TWO / X0**F21
+  120 IF(ERR.LT.ACC) GO TO 130
+       NX0 = INT((ERR/ACC)**(ONE/F21) * X0)
+       K = NB
+  130 NT = K
+C     print *,' logam requires k = ',k ,' with cutoff at x =',nx0+1
+      RETURN
       END
 
       FUNCTION TIDY(Z,ACC)

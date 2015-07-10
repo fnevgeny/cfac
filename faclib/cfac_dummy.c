871eda0382c5e948cafd4ae8e245cc48d5bf0e42
Evgeny Stabmulchik <fnevgeny@gmail.com>
2015-07-10 09:42:34 +0000

    Refer to README before abort()'ing when a CPC-licensed module is absent.

--- faclib/cfac_dummy.c
+++ faclib/cfac_dummy.c
@@ -4,13 +4,17 @@
 void cfac_dummy_dcoul(double a1, double a2, int a3, double a4, double *a5,
 		 double *a6, double *a7, double *a8, int *a9)
 {
-    fputs("This version of cFAC was compiled without COULCC module\n", stderr);
+    fputs(" * This version of cFAC was compiled without the COULCC module.\n",
+        stderr);
+    fputs(" * Please see the README file for instructions.\n", stderr);
     abort();
 }
 
 void cfac_dummy_cmultip(double a1, double a2, double a3, double a4, double a5,
 		  int a6, int a7, int a8, double *a9, int *a10)
 {
-    fputs("This version of cFAC was compiled without CMULTIP module\n", stderr);
+    fputs(" * This version of cFAC was compiled without the CMULTIP module.\n",
+        stderr);
+    fputs(" * Please see the README file for instructions.\n", stderr);
     abort();
 }

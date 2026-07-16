52f0e2204cd2ab0aa818cba529947eea195fa9e1
Evgeny Stambulchik <Evgeny.Stambulchik@weizmann.ac.il>
2026-07-16 12:32:29 +0300

    Fix the InitFac() prototype

--- sfac/sfac.c
+++ sfac/sfac.c
@@ -3066,7 +3066,7 @@ static void usage(FILE *fp, const char *progname) {
         progname);
 }
 
-static int InitFac() {
+static int InitFac(void) {
   gsl_ieee_env_setup();
 
   cfac = cfac_new();
